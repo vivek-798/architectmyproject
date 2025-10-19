5613ec5c9142d7447b37ba674c4fde28a59f85b0
vivek-798 <vivekshanigarapu0627@gmail.com>
2025-10-19 18:41:23 +0530

    make mobile responsive and added fotter ,socail media links

---
+++ architect/nav.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
+  });
+});
